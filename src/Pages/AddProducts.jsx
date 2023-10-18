1d24a85e5bf9f208cea074fa442fa6ea8a355b30
Rabiul37 <hmrabiul37@gmail.com>
2023-10-18 19:48:54 +0600

    add products form complete

--- src/Pages/AddProducts.jsx
+++ src/Pages/AddProducts.jsx
@@ -1,37 +1,59 @@
 import Navber from "../Components/Navber";
 
 const AddProducts = () => {
+  const handleForm = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const ProductImage = form.ProductImage.value;
+    const ProductName = form.ProductName.value;
+    const BrandName = form.BrandName.value;
+    const ProductType = form.ProductType.value;
+    const Price = form.Price.value;
+    const Description = form.Description.value;
+    const Rating = form.Rating.value;
+    console.log(
+      ProductImage,
+      ProductName,
+      BrandName,
+      ProductType,
+      Price,
+      Description,
+      Rating
+    );
+  };
   return (
     <div>
       <Navber></Navber>
-      <div className="hero min-h-screen bg-base-200">
+      <div className="hero min-h-screen bg-gray-700">
         <div className="hero-content flex-col ">
           <div className="text-center ">
-            <h1 className="text-5xl font-bold">Register now!</h1>
+            <h1 className="text-5xl font-bold text-gray-200">
+              Add your product !
+            </h1>
           </div>
           <div className="card flex-shrink-0 w-full  shadow-2xl bg-base-100">
-            <form className="card-body">
+            <form onSubmit={handleForm} className="card-body">
               <div className="lg:flex items-center gap-5">
                 <div className="form-control">
                   <label className="label">
-                    <span className="label-text">User Name</span>
+                    <span className="label-text">Product Image</span>
                   </label>
                   <input
                     type="text"
-                    name="name"
-                    placeholder="Your Name"
+                    name="ProductImage"
+                    placeholder="Enter Product Image link"
                     className="input input-bordered"
                     required
                   />
                 </div>
                 <div className="form-control">
                   <label className="label">
-                    <span className="label-text">User image</span>
+                    <span className="label-text">Product Name</span>
                   </label>
                   <input
                     type="text"
-                    name="imgURL"
-                    placeholder="Your image Link"
+                    name="ProductName"
+                    placeholder="Product Name"
                     className="input input-bordered"
                     required
                   />
@@ -40,31 +62,71 @@ const AddProducts = () => {
               <div className="lg:flex items-center gap-5">
                 <div className="form-control">
                   <label className="label">
-                    <span className="label-text">User Name</span>
+                    <span className="label-text">Brand Name</span>
                   </label>
                   <input
                     type="text"
-                    name="name"
-                    placeholder="Your Name"
+                    name="BrandName"
+                    placeholder="Brand Name"
                     className="input input-bordered"
                     required
                   />
                 </div>
                 <div className="form-control">
                   <label className="label">
-                    <span className="label-text">User image</span>
+                    <span className="label-text">Product Type</span>
                   </label>
                   <input
                     type="text"
-                    name="imgURL"
-                    placeholder="Your image Link"
+                    name="ProductType"
+                    placeholder="phone, computer, headphone, etc"
                     className="input input-bordered"
                     required
                   />
                 </div>
+              </div>
+              <div className="lg:flex items-center gap-5">
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Price</span>
+                  </label>
+                  <input
+                    type="text"
+                    name="Price"
+                    placeholder="Price"
+                    className="input input-bordered"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Description</span>
+                  </label>
+                  <input
+                    type="text"
+                    name="Description"
+                    placeholder="short description"
+                    className="input input-bordered"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="form-control w-1/3 m-auto">
+                <label className="label text-center">
+                  <span className="label-text">Rating</span>
+                </label>
+                <input
+                  type="text"
+                  name="Rating"
+                  placeholder="Rating"
+                  className="input input-bordered"
+                  required
+                />
               </div>
               <div className="form-control mt-6">
-                <button className="btn btn-primary">Add product</button>
+                <button className="btn bg-gray-700 text-gray-200">
+                  Add product
+                </button>
               </div>
             </form>
           </div>
